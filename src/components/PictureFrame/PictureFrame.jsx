b34a7d4e81f5e7bbf900a6061621669731cfb65f
rblis <ryanxp8@gmail.com>
2022-06-27 20:44:11 -0400

    added links to other projects

---
+++ src/components/PictureFrame/PictureFrame.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PictureFrame = () => {
+  return (
+    <div>PictureFrame</div>
+  )
+}
+
+export default PictureFrame
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,11 +4,14 @@ const HomePage = () => {
     return (
         <div>
             <div className="Centerpiece">
+                <h1>Portfolio</h1>                
+                <h2>Ryan Biswas</h2>
+                <hr></hr>
                 <ButtonLink link="https://rblis.github.io/PersonalMovieDatabase" text="Personal Movie Database (React)"/>
                 <ButtonLink link="https://rblis.github.io/Farmbase/" text="FarmBase (Angular)"/>
                 <ButtonLink link="https://uva-cs4730-sum22.github.io/project-typer-survivor/typer/index.html" text="HyperTyper (Phaser 3)"/>
-                <ButtonLink link="/about" text="About Me"/>
-                
+                <hr></hr>
+                <ButtonLink link="/about" text="About Me"/>                
             </div>  
         </div>
     )
